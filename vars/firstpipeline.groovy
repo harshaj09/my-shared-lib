37aff5f34f63b28ebaa59d823d9f0d8810807e33
harshaj09 <harsha.jonnadula99@gmail.com>
2024-08-16 16:24:34 +0530

    Update firstpipeline.groovy

--- vars/firstpipeline.groovy
+++ vars/firstpipeline.groovy
@@ -1,33 +1,35 @@
 import com.digitrends.builds.Calculator
-Calculator calculator = new Calculator(this)
-pipeline {
-    agent any
-    environment {
-        APPLICATION_NAME = "${pipelineParams.appName}"
-    }
-    stages {
-        stage ('AdditionStage') {
-            steps {
-                script {
-                    echo "sum of 2 numbers"
-                    println calculator.add()
-                    println calculator.addition(20,40)
-                    echo "My microservice name: ${APPLICATION_NAME}"
+def call(Map pipelineParams) {
+    Calculator calculator = new Calculator(this)
+    pipeline {
+        agent any
+        environment {
+            APPLICATION_NAME = "${pipelineParams.appName}"
+        }
+        stages {
+            stage ('AdditionStage') {
+                steps {
+                    script {
+                        echo "sum of 2 numbers"
+                        println calculator.add()
+                        println calculator.addition(20,40)
+                        echo "My microservice name: ${APPLICATION_NAME}"
+                    }
+                }
+            }
+            stage ('MultiplicationStage') {
+                steps {
+                    script {
+                        echo "Multiplication of 2 numbers"
+                        println calculator.multiplication(10,20)
+                    }
+                }
+            }
+            stage ('Mail') {
+                steps {
+                    echo "Sending an email"
                 }
             }
         }
-        stage ('MultiplicationStage') {
-            steps {
-                script {
-                    echo "Multiplication of 2 numbers"
-                    println calculator.multiplication(10,20)
-                }
-            }
-        }
-        stage ('Mail') {
-            steps {
-                echo "Sending an email"
-            }
-        }
-    }
+    }   
 }
