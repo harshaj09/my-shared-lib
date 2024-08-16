b0088a9e6e355236eef518452557f8b270c0125e
harshaj09 <harsha.jonnadula99@gmail.com>
2024-08-16 16:46:06 +0530

    Update firstpipeline.groovy

--- vars/firstpipeline.groovy
+++ vars/firstpipeline.groovy
@@ -1,31 +1,36 @@
 import com.digitrends.builds.Calculator
-
-Calculator calculator = new Calculator(this)
-pipeline {
-    agent {
-        label "slave"
-    }
-    stages {
-        stage ('AdditionStage') {
-            steps {
-                script {
-                    echo "sum of 2 numbers"
-                    println calculator.addition(10,20)
+def call(Map pipelineParams) {
+    Calculator calculator = new Calculator(this)
+    pipeline {
+        agent {
+            label "slave"
+        }
+        environment {
+            APP_NAME = "${pipelineParams.appName}"
+        }
+        stages {
+            stage ('AdditionStage') {
+                steps {
+                    script {
+                        echo "sum of 2 numbers"
+                        println calculator.addition(10,20)
+                        echo "Microservice is : ${APP_NAME}"
+                    }
+                }
+            }
+            stage ('MultiplicationStage') {
+                steps {
+                    script {
+                        echo "Multiplication of 2 numbers"
+                        println calculator.multiplication(10,20)
+                    }
+                }
+            }
+            stage ('Mail') {
+                steps {
+                    echo "Sending an email"
                 }
             }
         }
-        stage ('MultiplicationStage') {
-            steps {
-                script {
-                    echo "Multiplication of 2 numbers"
-                    println calculator.multiplication(10,20)
-                }
-            }
-        }
-        stage ('Mail') {
-            steps {
-                echo "Sending an email"
-            }
-        }
-    }
-}   
+    }  
+}
